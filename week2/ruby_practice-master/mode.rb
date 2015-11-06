1d6f102f4b39d54d6edb31d8e095f4a8cd2199fe
Jamie <gatorjuice@gmail.com>
2015-11-06 18:57:44 +0000

    got mode to work

--- week2/ruby_practice-master/mode.rb
+++ week2/ruby_practice-master/mode.rb
@@ -2,7 +2,15 @@
 def mode(array_of_input_numbers)
     frequency_of_numbers = Hash.new 0
     array_of_input_numbers.each { |number| frequency_of_numbers[number] += 1 }
-    frequency_of_numbers.max_by { |k, v| v }
+    max_frequency = frequency_of_numbers.values.max
+    frequency_of_numbers = frequency_of_numbers.select { |k, v| v == max_frequency }
+    mode_numbers = []
+    frequency_of_numbers.each_pair { |k, v| mode_numbers << k }
+    if mode_numbers.length <= 1
+        mode_numbers[0]
+    else
+        mode_numbers
+    end
 end
 # Driver code
 p mode([2, 4, 5, 2]) # should be 2
